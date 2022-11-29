05dd9f3f8645f9513e2a88ddea96912a3b3af02d
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-29 11:18:28 -0500

    Create of the script that create a database and new user

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- Create of the database and new user, passwd and privilates.
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
