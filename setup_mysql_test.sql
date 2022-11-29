9ad51c6f30f0b265ffcf98d87fe026f8a1f9bed1
ManuRodriguezC <estebanclimb@gmail.com>
2022-11-29 11:30:01 -0500

    Create a database test and a new user

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- Created of a database and new user in the localhost.
+
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON `hbnb_test_bd`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
