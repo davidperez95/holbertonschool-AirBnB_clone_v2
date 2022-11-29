7cbffa0e50681a7ef65b6b02e792ec2f95cac516
David Perez <daperezcruz@gmail.com>
2022-11-29 13:55:37 -0500

    task 6 first points

--- models/base_model.py
+++ models/base_model.py
@@ -2,10 +2,25 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, ForeignKey
+
+
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    """
+    Class attributes:
+        id: string(60), primary key
+        created_at: default value is the current datetime (use datetime.utcnow())
+        updated_at: default value is the current datetime (use datetime.utcnow())
+    """
+    id = Column(String(60), nullable=False, unique=True, primary_key=True)
+    created_at = Column(nullable=False, default=datetime.utcnow())
+    updated_at = Column(nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -13,13 +28,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
         else:
             kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             del kwargs['__class__']
+            for key, value in kwargs.items():
+                setattr(self, key, value)
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -31,6 +47,7 @@ class BaseModel:
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
@@ -41,4 +58,10 @@ class BaseModel:
                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if "_sa_instance_state" in dictionary.keys():
+            dictionary.pop("_sa_instance_state")
         return dictionary
+
+    def delete(self):
+        """delete the current instance from the storage"""
+        
