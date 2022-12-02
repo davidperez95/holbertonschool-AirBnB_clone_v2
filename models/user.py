88ded9997386a3bd862e7b0687b7395f1324b61f
Danielxxdxdxd <xxdxdxd332@gmail.com>
2022-12-02 09:15:00 -0500

    user

--- models/user.py
+++ models/user.py
@@ -16,8 +16,8 @@ class User(BaseModel, Base if (getenv('HBNB_TYPE_STORAGE') == "db")
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship('Place', backref='user', cascade='all, delete')
-        reviews = relationship('Review', backref='user', cascade='all, delete')
+        places = relationship('Place', backref='user', cascade='delete')
+        reviews = relationship('Review', backref='user', cascade='delete')
     else:
         email = ''
         password = ''
