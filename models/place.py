7b1c0122258b61862c5f1d23101a3c9b6d272c58
David Perez <daperezcruz@gmail.com>
2022-12-02 22:02:13 -0500

    task 10

--- models/place.py
+++ models/place.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.amenity import Amenity
 from sqlalchemy import Column, ForeignKey, String, Float, Integer
 from sqlalchemy.orm import relationship
+from sqlalchemy import Table
 import models
+
+
+association_table = Table("place_amenity", Base.metadata,
+                            Column("place_id", String(60),
+                            ForeignKey("places.id"),
+                            primary_key=True, nullable=False),
+                            Column("amenity_id", String(60),
+                            ForeignKey("amenities.id"),
+                            primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -20,8 +32,26 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     reviews = relationship('Review', backref='place',cascade='all, delete')
+    amenities = relationship("Amenity", secondary="place_amenity", viewonly=True)
+
+    amenity_ids = []
 
     def __init__(self, *args, **kwargs):
         """"""
         super().__init__(*args, **kwargs)
-    
+
+    if models.storage_type != "db":
+        @property
+        def amenities(self):
+            """"""
+            amenity_list = []
+            for amenity in list(models.storage.all(Amenity).values()):
+                if amenity.id in self.amenity_ids:
+                    amenity_list.append(amenity)
+            return amenity_list
+        
+        @amenities.setter
+        def amenities(self, value):
+            """"""
+            if type(value) == Amenity:
+                self.amenity_ids.append(value.id)
