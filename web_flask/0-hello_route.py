aac8042d07b9f4cb7f74b89940f8a666ba8f6143
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-06 13:47:01 -0500

    Created the web flask, learning to start flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 from flask import Flask
+"""
+This module start the conection with flask,
+for conect the web con database
+"""
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def slash():
+    """This method return the messege of the page /"""
     return "Hello HBNB!"
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
