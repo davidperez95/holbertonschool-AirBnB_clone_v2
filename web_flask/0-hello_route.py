54e2ffb0d310778eac10143f459d9ec48be39956
ManuRodriguezC <estebanclimb@gmail.com>
2022-12-06 13:47:38 -0500

    Created the web flask, learning to start flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-from flask import Flask
 """
 This module start the conection with flask,
 for conect the web con database
 """
+from flask import Flask
+
 
 app = Flask(__name__)
 
