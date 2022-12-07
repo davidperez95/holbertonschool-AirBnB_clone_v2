453e5438f3a4d6b84be62add3cc852e5396b7a93
David Perez <daperezcruz@gmail.com>
2022-12-07 18:16:17 -0500

    task 4 display a number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This script display “Hello HBNB!” on '/' and
+“HBNB” on /hbnb route
+"C <text>" on /c/ rourte and the text variable
+"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    """Return “Hello HBNB!” on the console"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Return "HBNB" on the console on /hbnb"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def display_text(text):
+    """Return "C" + <text> variable on /c/"""
+    new_string = text
+    if '_' in new_string:
+        new_string = text.replace('_', ' ')
+    return 'C {}'.format(new_string)
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def display_text_default(text):
+    """Return "Python" with the default value is cool"""
+    new_string = text
+    if '_' in new_string:
+        new_string = text.replace('_', ' ')
+    return 'Python {}'.format(new_string)
+
+
+@app.route('/number/<int:n>')
+def display_int(n):
+    """Return n is a number only if n is type int"""
+    if type(n) is int:
+        return '{:d} is a number'.format(n)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
