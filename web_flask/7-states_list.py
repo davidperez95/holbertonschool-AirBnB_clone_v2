ac07b842e5edf2b11c1e81726fd0797203792567
David Perez <daperezcruz@gmail.com>
2022-12-09 12:43:10 -0500

    task 7 con web framework

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Starts a flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Display a list of States on a HTML page"""
+    states = storage.all('State')
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exeption):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
