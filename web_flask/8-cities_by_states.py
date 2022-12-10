697107a7a2a4236f863ee6529d0cc2e1ca985e03
David Perez <daperezcruz@gmail.com>
2022-12-09 21:38:35 -0500

    task 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Starts a flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+
+app = Flask(__name__)
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def states_list():
+    """Display a list of States on a HTML page"""
+    states = storage.all(State)
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exeption):
+    """remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
