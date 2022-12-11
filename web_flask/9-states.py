0ba2e007058e62c083fd79469b7da5a91fd28b5d
David Perez <daperezcruz@gmail.com>
2022-12-11 12:47:23 -0500

    task 10 fix on both files

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -12,11 +12,11 @@ app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
+@app.route('/states/<id>', strict_slashes=False)
+def states(id=None):
     """display the states and cities listed in alphabetical order"""
     states = storage.all(State)
-    if state_id is not None:
+    if id is not None:
         state_id = 'State.' + state_id
     return render_template('9-states.html', states=states, state_id=state_id)
 
